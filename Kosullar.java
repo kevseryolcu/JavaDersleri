1697f0f07e254cbb4c4c33ec56b517923d6c4486
Kevser Yolcu <kyolcu@gtu.edu.tr>
2022-01-28 22:04:42 +0300

    ders 3

---
+++ Kosullar.java
@@ -0,0 +1,57 @@
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Kosullar {
+    public static void main(String[] args) {
+        int sayi = 1;
+
+        // kosullu ifadeler
+        /*if(sayi == 2) {
+            System.out.println("bu sayi 2dir");
+        }
+        else if(sayi == 1) {
+            System.out.println("bu sayi 1dir");
+        }
+        else {
+            System.out.println("bu sayi 2 degildir");
+        }
+
+        double dsayi = 1.5;
+        if( dsayi > 0) {
+            System.out.println("bu sayi sifirdan büyüktür");
+        }
+        else if(dsayi < 0) {
+            System.out.println("bu sayi sifirdan kücüktür");
+        }
+        else {
+            System.out.println("bu sayi sifirdir");
+        }
+
+        sayi = -9;
+        if(sayi >= 0 && sayi < 10) {
+            System.out.println("bu sayi sifir ile 10 arasindadir");
+        }*/
+
+        // random
+        /*double randomsayi = Math.random()*20;
+        System.out.println((int)randomsayi);
+        randomsayi = Math.random()*20;
+        System.out.println((int)randomsayi);
+        randomsayi = Math.random()*20;
+        System.out.println((int)randomsayi);*/
+
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Bir tane integer sayi girniz");
+        int kullaniciSayisi = scanner.nextInt();
+        System.out.println("Girdiginiz sayi: " + kullaniciSayisi);
+        
+        System.out.println("Bir tane double sayi giriniz");
+        double kullaniciSayisi2 = scanner.nextDouble();
+        System.out.println("Girdiginiz sayi: " + kullaniciSayisi2);
+        
+        System.out.println("Bir tane string giriniz");
+        scanner.nextLine();
+        String kullaniciStringi = scanner.nextLine();
+        System.out.println("Girdiginiz satir: " + kullaniciStringi);*/
+    }
+}
---
+++ SayiTahminOyunu.java
@@ -0,0 +1,19 @@
+import java.lang.Math;
+import java.util.Scanner;
+
+
+public class SayiTahminOyunu {
+    public static void main(String[] args) {
+        double randomsayi = Math.random()*10;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bir tane integer sayi girniz");
+        int kullaniciSayisi = scanner.nextInt();
+        System.out.println("Girdiginiz sayi: " + kullaniciSayisi);
+
+        // kosullu ifadeler
+        // mantık operatorleri
+        // karsilastirma operatorleri
+    }
+    
+}
